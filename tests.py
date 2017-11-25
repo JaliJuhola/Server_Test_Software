05995b3d63656404f5c7da3c0eb196ec0a9bef7a
Jali Juhola <jalijuhola@gmail.com>
2017-11-25 20:58:06 +0200

    testclass added on more

--- tests.py
+++ tests.py
@@ -1,9 +1,34 @@
 import requests
 import json
+import threading
+import time
 
 class testClass:
+    outputValues = []
+    
+    def runWithThread(command):
+        status = True
+        while status:
+            r = requests.get(command["location"], params=command["queryStrings"], headers=command["headers"])
+            if r.status_code == 200:
+                testClass.outputValues.append("Call: " + r.url + " succeed")
+                status = True
+                time.sleep(command.recursive_intervall)
+            else:
+                testClass.outputValues.append("Call: " + r.url+ " failed")
+                status = False
+
     def execute():
-        params = json.load(open('callstack.json'))
-        return params
+        commands = json.load(open('callstack.json'))
+        for call in commands["calls"]:
+            if call["recursive"]:
+                threading.Thread(target= testClass.runWithThread, args=(call))
+            else:
+                r = requests.get(call["location"], params=call["queryStrings"], headers=call["headers"])
+                if r.status_code == 200:
+                    testClass.outputValues.append("Call: " + r.url + " succeed")
+                else:
+                    testClass.outputValues.append("Call: " + r.url+ " failed")
+
 
 
