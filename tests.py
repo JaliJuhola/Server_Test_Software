e476b38447bf1d08b90ff8137a8c541989fb4bd8
Jali Juhola <jalijuhola@gmail.com>
2017-11-25 21:12:10 +0200

    hotfix for config and multiple testing runs supported

--- tests.py
+++ tests.py
@@ -13,23 +13,29 @@ class testClass:
             if r.status_code == 200:
                 testClass.outputValues.append("Call: " + r.url + " succeed")
                 status = True
-                time.sleep(command.recursive_intervall)
+                time.sleep(command['recursive_intervall'])
             else:
                 testClass.outputValues.append("Call: " + r.url+ " failed")
                 status = False
 
     def execute():
         commands = json.load(open('callstack.json'))
-        for call in commands["calls"]:
-            if call["recursive"]:
-                threading.Thread(target= testClass.runWithThread, args=(call))
-            else:
-                r = requests.get(call["location"], params=call["queryStrings"], headers=call["headers"])
-                if r.status_code == 200:
-                    testClass.outputValues.append("Call: " + r.url + " succeed")
+        i = 0
+        while i < commands['run_amount']:
+            for call in commands["calls"]:
+                if call["recursive"]:
+                    threading.Thread(target= testClass.runWithThread, args=(call))
                 else:
-                    testClass.outputValues.append("Call: " + r.url+ " failed")
+                    r = requests.get(call["location"], params=call["queryStrings"], headers=call["headers"])
+                    if r.status_code == 200:
+                        testClass.outputValues.append("Call: " + r.url + " succeed")
+                    else:
+                        testClass.outputValues.append("Call: " + r.url+ " failed")
+                time.sleep(commands['wait_time'])
+
+        i = i + 1
         writeResults(commands['output_file'])
+        return testClass.outputValues
 
     def writeResults(filename):
         file = open(filename,”w”)
